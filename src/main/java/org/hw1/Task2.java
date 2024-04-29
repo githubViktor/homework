5084bfd5d2f8a300dc49f18de1ceff04a294e681
viktor <viktor-21.ru@yandex.ru>
2024-04-29 16:12:58 +1000

    сделано Task2 из hw1

---
+++ src/main/java/org/hw1/Task2.java
@@ -0,0 +1,15 @@
+package org.hw1;
+
+public class Task2 {
+    public static void checkSign(int a, int b, int c){
+        if(a+b+c >= 0){
+            System.out.println("Сумма положительная");
+        }else{
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void main(String[] args) {
+        checkSign(1,2,5);
+        checkSign(-1,2,-234234);
+    }
+}
