a0e61746a8223f8866d0b5f699ae92e60afd4218
viktor <viktor-21.ru@yandex.ru>
2024-04-29 15:56:01 +1000

    сделано Task1 из hw1

---
+++ src/main/java/org/hw1/Task1.java
@@ -0,0 +1,12 @@
+package org.hw1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Task1 {
+    public static void greetings() {
+        System.out.print("Hello\nWorld\nfrom\nJava");
+    }
+    public static void main(String[] args) {
+        greetings();
+        }
+}
