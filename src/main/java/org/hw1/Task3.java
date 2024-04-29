8e71cff7377fff3f105cad4ff57f90d42a141b09
viktor <viktor-21.ru@yandex.ru>
2024-04-29 17:11:24 +1000

    сделано Task3

---
+++ src/main/java/org/hw1/Task3.java
@@ -0,0 +1,19 @@
+package org.hw1;
+public class Task3 {
+    public  static void selectColor(){
+        int data = 15;
+        if(data < 10){
+            System.out.println("Красный");
+        };
+        if( (data >= 10) & (data <= 20) ){
+            System.out.println("Желтый");
+        };
+        if(data > 20){
+            System.out.println("Зеленый");
+        };
+
+    }
+    public static void main(String[] args) {
+        selectColor();
+    }
+}
