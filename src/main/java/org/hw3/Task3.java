e8dc1b76e6f88cf5d43d06353af20773e5c92d15
viktor <viktor-21.ru@yandex.ru>
2024-04-30 01:01:37 +1000

    выполнено Task3

---
+++ src/main/java/org/hw3/Task3.java
@@ -0,0 +1,17 @@
+package org.hw3;
+
+public class Task3 {
+
+    public static void Square(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Square(7);
+    }
+}
